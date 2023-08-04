913a80f8d3f2a0f0207ac3be7dc554c1f99183b6
Durga Kumar <95281727+hackdurga@users.noreply.github.com>
2023-08-04 23:04:12 +0530

    Update constructor for the app component

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph:false
+
     };
   }
 
